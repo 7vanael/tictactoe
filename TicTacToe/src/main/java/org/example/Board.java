e130f758230b9519f8a3681c57a6c0ac37feb2d7
Kristi <kristi.n.norris777@gmail.com>
2024-12-12 13:19:37 -0700

    Added players, board in progress

---
+++ TicTacToe/src/main/java/org/example/Board.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Board {
+    private Player[][] board;
+
+    public Board(){
+        board = new Player[3][3];
+    }
+
+    public void displayBoard() {
+        System.out.println();
+    }
+}
---
+++ TicTacToe/src/main/java/org/example/Game.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.List;
+
+public class Game {
+    private Board board;
+    private Scanner scanner;
+
+
+    public Game(Scanner scanner){
+        this.scanner = scanner;
+        this.board = new Board();
+    }
+
+    public void play(){
+
+    }
+
+}
