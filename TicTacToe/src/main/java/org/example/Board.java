3c66be11bcc7cb566233f20e285cd85b320bfd57
Kristi <kristi.n.norris777@gmail.com>
2024-12-16 22:01:34 -0700

    rules of the board

--- TicTacToe/src/main/java/org/example/Board.java
+++ TicTacToe/src/main/java/org/example/Board.java
@@ -9,5 +9,69 @@ public class Board {
 
     public void displayBoard() {
         System.out.println();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == null) {
+                    System.out.print("- ");
+                } else {
+                    System.out.print(board[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    public boolean takeSquare(int row, int col, Player player) {
+        // Check if move is valid
+        if (row < 0 || row > 2 || col < 0 || col > 4) {
+            return false;
+        }
+
+        if (board[row][col] != null) {
+            return false;
+        }
+
+        board[row][col] = player;
+        return true;
+    }
+
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkWin(Player player) {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == player && board[1][j] == player && board[2][j] == player) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true;
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true;
+        }
+
+        return false;
     }
 }
