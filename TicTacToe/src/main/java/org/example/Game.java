bc7f69323189c41a889c306d523343bc7723140b
Kristi <kristi.n.norris777@gmail.com>
2025-01-09 16:23:43 -0700

    Pseudocode

--- TicTacToe/src/main/java/org/example/Game.java
+++ TicTacToe/src/main/java/org/example/Game.java
@@ -15,10 +15,34 @@ public class Game {
 
     public void play(){
         UserInterface userInterface = new UserInterface();
+        /*
+          Print Welcome screen
+            Do
+                Do
+                  Turn(if player == computer){
+                      Computer turn:
+                        Check if opening move
+                        Check if win possible
+                        Check if loss imminent (to block a move)
+                            Select a play
+                            verify valid play
+                  else
+                      Prompt for user input
+                        verify valid input
+                        verify valid play
+                  }
+                  Record the play
+                  Board.takeSquare
+                  Print the board
+                  Board.displayBoard
+                  Check win conditions (including draw)
+                    Set gameOver = true if draw or win/loss
+                while(!gameOver)
+                Declare winner
+                Prompt user: Play again?
+                    set sessionOver
+            while(!sessionOver)
+         */
     }
-
-
-
-
 
 }
